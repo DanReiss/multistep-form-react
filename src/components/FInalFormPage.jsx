ac8d57c55420d5e77629bd1891235b5ae9dd7eb0
Danilo dos Reis Amaral <danreis.ctt@gmail.com>
2023-04-26 09:14:46 -0300

    corrigir grid na página de finalização
    corrigi um erro no qual a página de agradecimento, 'FinalFormPage.jsx', estava quebrando o layout de grid

--- src/components/FInalFormPage.jsx
+++ src/components/FInalFormPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
+import StepsViewBar from "./StepsViewBar";
 
-function FinalFormPage({username, setVisualEl}) {
+function FinalFormPage({username, setVisualEl, stepsMap}) {
 
   useEffect(()=> {
     setVisualEl(["balloons"]);
@@ -17,6 +18,7 @@ function FinalFormPage({username, setVisualEl}) {
 
   return (
     <div className="form_wrapper">
+        <StepsViewBar stepsMap={stepsMap}/> 
         <section className="form_container">
             <div className="success_box">
               <SucessMessage/>
